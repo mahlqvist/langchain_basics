4c91f4b301d9820c7df6baf265e2a6fcca61b563
mahlqvist <mahlqvist@msn.com>
2025-06-17 22:17:23 +0200

    updating output parsers and adding document loaders

--- examples/03_output_parsers.py
+++ examples/03_output_parsers.py
@@ -1,13 +1,12 @@
 import os
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
 
 env_path = os.path.join(os.getcwd(), "config", ".env")
 
 _ = load_dotenv(dotenv_path=env_path)
-
 
 api_key = os.getenv("OPENAI_API_KEY")
 
@@ -19,3 +18,37 @@ llm = ChatOpenAI(
     temperature=0.3,
     top_p=0.9,
 )
+
+template = """
+You are a film scholar and expert in movies. 
+
+Generate information about "{movie_name}" in JSON format.
+
+Return ONLY a JSON object with no text before or after. The JSON must have these keys:
+
+Output valid JSON in exactly this format:
+{{
+	"title": "movie title",
+  	"director": "director name",
+  	"year": year as number,
+  	"genre": "movie genre"
+}}
+
+If any field doesn't apply or lacks sufficient information, use "N/A"
+
+Your entire response must be valid JSON. Do not include any text outside the JSON object in your response.
+"""
+
+prompt = PromptTemplate.from_template(template)
+
+# combined chain using LangChain Expression Language (LCEL)
+chain = prompt | llm | JsonOutputParser()
+
+movie_name = "The Matrix"
+result = chain.invoke({
+    "movie_name": movie_name,
+})
+
+print(f"Type: {type(result)}")
+print(f"Title: {result['title']}")
+print(f"Genre: {result['genre']}")
---
+++ examples/04_document_loaders.py
@@ -0,0 +1,66 @@
+"""
+In order to use document loaders:
+
+pip install -qU langchain-community pypdf beautifulsoup4 lxml
+
+pypdf is for PyPDFLoader
+bs4 is for WebBaseLoader
+lxml if you want a better html parser
+"""
+import os
+import re
+from langchain_community.document_loaders import PyPDFLoader, WebBaseLoader, UnstructuredPDFLoader
+
+pdf_file_path = os.path.join(os.getcwd(), "data", "machine_minds.pdf")
+
+# Using the PyPDFLoader
+pdf_loader = PyPDFLoader(pdf_file_path)
+
+pages = []
+
+# Each document is a Document object with page_content and metadata
+documents = pdf_loader.load()
+
+# Inspect the extracted documents
+for doc in documents:
+	print(f"Type: {doc.metadata['category']}, Content: {doc.page_content[:100]}...")
+
+
+# Using WebBaseLoader
+web_loader = WebBaseLoader("https://www.uu.se/en/centre/crb/news/archive/2024-09-23-exploring-artificial-consciousness-drawing-inspiration-from-the-human-brain")
+web_loader.default_parser = "lxml"
+web_loader.requests_kwargs = {"headers": {"User-Agent": "Mozilla/5.0"}} 
+data = web_loader.load()
+
+def sanitize_web_text(raw_text: str) -> str:
+    """
+    Remove repeated newlines
+    Remove leading/trailing whitespace from each line
+    """
+    raw_text = re.sub(r'\n{2,}', '\n\n', raw_text)
+    lines =  [line.strip() for line in raw_text.splitlines()]
+    text = "\n".join(lines)
+    return text
+
+web_data = {
+    "metadata": data[0].metadata,
+    "content": sanitize_web_text(data[0].page_content)
+}
+
+print(f"SOURCE: {web_data['metadata']['source']}")
+print(f"CONTENT: {web_data['content'][:100]}")
+
+
+# Using UnstructuredPDFLoader to extract images and tables
+adv_pdf_loader = UnstructuredPDFLoader(
+	pdf_file_path,
+	mode="elements",
+	infer_table_structure=True,
+)
+pages = []
+
+documents = adv_pdf_loader.load()
+
+# Inspect the extracted documents
+for doc in documents:
+    print(f"Type: {doc.metadata['category']}, Content: {doc.page_content[:100]}...")

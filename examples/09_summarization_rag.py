c7048dbdc83a52a310308297e3a0289e5496d772
mahlqvist <mahlqvist@msn.com>
2025-06-24 15:10:41 +0200

    removed some space

--- examples/09_summarization_rag.py
+++ examples/09_summarization_rag.py
@@ -34,7 +34,6 @@ embeddings = OpenAIEmbeddings(
 	model="text-embedding-3-small",
 )
 
-
 url = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/6JDbUb_L3egv_eOkouY71A.txt'
 
 file_name = "company_policies.txt"
